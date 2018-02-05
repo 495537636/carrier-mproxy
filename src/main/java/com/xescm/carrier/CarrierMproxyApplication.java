eed6c39aa9025f4d7f8b48a132aaa92acc752778
developer <developer@github.com>
2018-02-05 13:52:43 +0800

    新建工程

---
+++ src/main/java/com/xescm/carrier/CarrierMproxyApplication.java
@@ -0,0 +1,14 @@
+package com.xescm.carrier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class CarrierMproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrierMproxyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xescm/carrier/CarrierMproxyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xescm.carrier;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarrierMproxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
